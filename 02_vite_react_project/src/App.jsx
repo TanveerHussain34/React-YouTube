851294fd81ecb25f084543ad1218471b3e7c7fcb
Tanveer Hussain <maliktanveerhussain459@gmail.com>
2024-08-10 18:40:36 +0500

    First React Project using VITE

---
+++ 02_vite_react_project/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <h1>
+      This is the React Project and created by Tanveer Hussain using{" "}
+      <i>npm create vite@latest</i>.
+    </h1>
+  );
+}
+
+export default App;
