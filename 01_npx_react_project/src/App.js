a3eca5ac4dd74d7969a744b1a1fbb15288d868b3
Tanveer Hussain <maliktanveerhussain459@gmail.com>
2024-08-10 18:27:36 +0500

    First React Project using NPX

---
+++ 01_npx_react_project/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <h1>
+      This is the React Project and created by Tanveer Hussain using "npx
+      create-react-app".
+    </h1>
+  );
+}
+
+export default App;
