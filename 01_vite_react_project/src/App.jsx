8ccf12aefe3ce29b20522248b5d3b4437a5ba9c8
Tanveer Hussain <maliktanveerhussain459@gmail.com>
2024-08-12 18:03:09 +0500

    React Working Behind the Scenes

--- 01_vite_react_project/src/App.jsx
+++ 01_vite_react_project/src/App.jsx
@@ -1,15 +1,16 @@
 import Greeting from "./Greeting";
 
 function App() {
+  const userName = "Tanveer";
   return (
     <>
       <Greeting />
       <h1>
-        This is the React Project created by Tanveer Hussain using{" "}
+        This is the React Project created by {userName} Hussain using{" "}
         <i>npm create vite@latest</i>.
       </h1>
     </>
   );
-}
+} // {userName} is evaluated expression and we can use it in JSX but the expression to be evaluated like (if(condition){console.log(etc)}), can't be used
 
 export default App;
--- 01_vite_react_project/src/main.jsx
+++ 01_vite_react_project/src/main.jsx
@@ -1,9 +1,39 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+function MyApp() {
+  return (
+    <div>
+      <h1>Chai aur React</h1>
+    </div>
+  );
+}
+
+// Our Custom Tree Syntax
+// const reactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   child: "Click Me",
+// };
+
+const anotherElement = (
+  <a href="https://google.com" target="_black">
+    Click Me to visit Google
+  </a>
 );
+
+const userName = "Tanveer";
+
+// React Tree Syntax
+const reactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "Go to Google | ",
+  userName // variable
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(reactElement);
