8e1fdec5afdacef81444488da9e6209dd7663d79
Tanveer Hussain <maliktanveerhussain459@gmail.com>
2024-08-15 21:29:52 +0500

    Solved Interview based Scenario

--- 02_counterProject/src/App.jsx
+++ 02_counterProject/src/App.jsx
@@ -10,6 +10,14 @@ function App() {
     if (counter < 20) {
       counter = counter + 1;
       setCounter(counter);
+      // setCounter(counter);
+      // setCounter(counter);
+      // setCounter(counter); // useState updates UI and variables(counter) in batches(groups). So, clicking on add button will not update counter 4 times instead it will just update it once(batch update).
+
+      // setCounter((counter) => counter + 1);
+      // setCounter((counter) => counter + 1);
+      // setCounter((counter) => counter + 1);
+      // setCounter((counter) => counter + 1); // Now if we want to update counter more than 1 times that is not usual, setCounter accepts call back in return and updates variable every time. Now clicking on add button will increase counter value by 4 by updating counter every time.
     } else {
       alert("Maximum counter value reached!");
     }
@@ -20,6 +28,14 @@ function App() {
     if (counter > 0) {
       counter = counter - 1;
       setCounter(counter);
+      // setCounter(counter);
+      // setCounter(counter);
+      // setCounter(counter);
+
+      // setCounter((counter) => counter - 1);
+      // setCounter((counter) => counter - 1);
+      // setCounter((counter) => counter - 1);
+      // setCounter((counter) => counter - 1);
     } else {
       alert("Minimum counter value reached!");
     }
