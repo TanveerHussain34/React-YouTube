32f5fd8137909c79160e5b529b9fe62e58594f42
Tanveer Hussain <maliktanveerhussain459@gmail.com>
2024-08-17 23:56:14 +0500

    Working on Password Generator

---
+++ 05_pwdGenerator/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <h1
+      className="text-4xl font-bold text-center mt-8"
+      style={{ color: "#61DAFB" }}
+    >
+      Password Generator using React
+    </h1>
+  );
+}
+
+export default App;
