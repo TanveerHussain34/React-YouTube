8d4f2603320859daf34251affad5f72427477c5a
Tanveer Hussain <maliktanveerhussain459@gmail.com>
2024-10-02 17:13:15 +0500

    Theme Switcher Project

---
+++ 09_themeSwitcher/README.md
@@ -0,0 +1,3 @@
+# React-Theme-Switcher
+
+This project is a fully functional Theme Switcher built with React, using the Context API to manage global state and Tailwind CSS for styling.
---
+++ 09_themeSwitcher/src/App.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Card from "./components/Card";
+import ThemeBtn from "./components/ThemeBtn";
+import { ThemeContext } from "./context/Theme";
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light");
+  const lightTheme = () => setThemeMode("light");
+  const darkTheme = () => setThemeMode("dark");
+  useEffect(() => {
+    document.querySelector("html").classList.remove("dark", "light");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+  return (
+    <ThemeContext.Provider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeContext.Provider>
+  );
+}
+
+export default App;
